050832abedc0529a423a1cc801b624d9898fc7f0
paoqi1997 <604869221@qq.com>
2021-01-20 20:45:42 +0800

    微调

--- raw-examples/learnopengl/util/test.cpp
+++ raw-examples/learnopengl/util/test.cpp
@@ -1,16 +1,26 @@
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 
 #include "util.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int main()
 {
+    // currentpath: C:\Windows\System32\drivers\etc
     pqwan::File oFile("C:\\Windows\\System32\\drivers\\etc\\hosts");
 
     cout << oFile.getFileContent() << endl;
 
-    system("pause");
+#ifdef _WIN32
+    const char *cmd = "pause";
+    int status = std::system(cmd);
+    if (status != 0) {
+        std::printf("Failed to call std::system(\"%s\")\n", cmd);
+    }
+#endif
+
     return 0;
 }
