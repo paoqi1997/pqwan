cc66de0d2845495c2429723a1ff93c336473f4d5
paoqi1997 <604869221@qq.com>
2024-11-11 22:27:18 +0800

    fix(raw-examples): [game-backend] UNIX domain sockets

--- raw-examples/game-backend/gameLogic/main.go
+++ raw-examples/game-backend/gameLogic/main.go
@@ -1,10 +1,85 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "io"
+    "net"
+    "os"
+    "os/signal"
+    "strings"
+    "syscall"
 )
 
+const (
+    G_Sock string = "/tmp/local-cache.sock"
+)
+
+func OnRead(conn net.Conn) {
+    size := 1024 // 1K
+    buffer := make([]byte, size)
+
+    for {
+        _, err := conn.Read(buffer)
+
+        if err != nil {
+            if err == io.EOF {
+                fmt.Printf("Server %s closed.\n", conn.RemoteAddr().String())
+                os.Exit(0)
+            } else {
+                fmt.Printf("Read: %v\n", err)
+            }
+
+            break
+        }
+    }
+}
+
+func OnExit() {
+    ch := make(chan os.Signal, 1)
+
+    signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+
+    go func(ch chan os.Signal) {
+        <-ch
+
+        fmt.Println("Exited.")
+
+        os.Exit(0)
+    }(ch)
+}
+
 func main() {
-    s := "Hello, World!"
-    fmt.Println(s)
+    conn, err := net.Dial("unix", G_Sock)
+
+    if err != nil {
+        fmt.Printf("net.Dial: %v\n", err)
+        return
+    }
+
+    defer conn.Close()
+
+    OnExit()
+
+    go OnRead(conn)
+
+    input := bufio.NewReader(os.Stdin)
+
+    for {
+        line, err := input.ReadString('\n')
+
+        if err != nil {
+            fmt.Printf("ReadString: %v\n", err)
+            break
+        }
+
+        line = strings.TrimRight(line, "\r\n")
+
+        _, err = conn.Write([]byte(line))
+
+        if err != nil {
+            fmt.Printf("Write: %v\n", err)
+            break
+        }
+    }
 }
--- raw-examples/game-backend/localCache/main.go
+++ raw-examples/game-backend/localCache/main.go
@@ -2,9 +2,88 @@ package main
 
 import (
     "fmt"
+    "io"
+    "net"
+    "os"
+    "os/signal"
+    "syscall"
 )
 
+const (
+    G_Sock string = "/tmp/local-cache.sock"
+)
+
+var (
+    G_Counter uint32 = 0
+)
+
+func OnAccept(conn net.Conn, connID uint32) {
+    defer conn.Close()
+
+    size := 8192 // 8K
+    buffer := make([]byte, size)
+
+    for {
+        nBytes, err := conn.Read(buffer)
+
+        if err != nil {
+            if err == io.EOF {
+                fmt.Printf("Connection %s/%d closed.\n", conn.RemoteAddr().String(), connID)
+            } else {
+                fmt.Printf("Read: %v\n", err)
+            }
+
+            break
+        }
+
+        msg := string(buffer[:nBytes])
+
+        fmt.Printf("Connection %s/%d: %s\n", conn.RemoteAddr().String(), connID, msg)
+    }
+}
+
+func OnExit() {
+    ch := make(chan os.Signal, 1)
+
+    signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+
+    go func(ch chan os.Signal) {
+        <-ch
+
+        err := os.Remove(G_Sock)
+
+        if err != nil {
+            fmt.Printf("os.Remove: %v\n", err)
+        }
+
+        fmt.Println("Exited.")
+
+        os.Exit(0)
+    }(ch)
+}
+
 func main() {
-    s := "Hello, World!"
-    fmt.Println(s)
+    listener, err := net.Listen("unix", G_Sock)
+
+    if err != nil {
+        fmt.Printf("net.Listen: %v\n", err)
+        return
+    }
+
+    defer listener.Close()
+
+    OnExit()
+
+    for {
+        conn, err := listener.Accept()
+
+        if err != nil {
+            fmt.Printf("Accept: %v\n", err)
+            continue
+        }
+
+        G_Counter++
+
+        go OnAccept(conn, G_Counter)
+    }
 }
