80c21b3a73e5877aa4630b2183ddf3c788124375
paoqi1997 <604869221@qq.com>
2025-02-28 16:40:03 +0800

    fix(raw-examples): [CppWithLua] stopwatch support linux

--- raw-examples/CppWithLua/stopwatch/xmake.lua
+++ raw-examples/CppWithLua/stopwatch/xmake.lua
@@ -7,5 +7,12 @@ target("app")
     add_includedirs("../lua-5.4.7/src")
     add_links("lua54")
     if is_plat("windows") then
-        add_linkdirs("../xbuild-lua/windows/x64/debug")
+        local rel_path = "../xbuild-lua/windows/x64/debug"
+        local abs_path = path.absolute(rel_path)
+        add_linkdirs(abs_path)
+    elseif is_plat("linux") then
+        local rel_path = "../xbuild-lua/linux/x86_64/debug"
+        local abs_path = path.absolute(rel_path)
+        add_linkdirs(abs_path)
+        add_rpathdirs(abs_path)
     end
