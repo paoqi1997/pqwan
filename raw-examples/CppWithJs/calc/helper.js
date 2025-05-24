adabaf2a02eae6c3f3576f4e4062393b268bdb34
paoqi1997 <604869221@qq.com>
2025-05-24 19:51:37 +0800

    fix(raw-examples): [CppWithJs] update calc

--- raw-examples/CppWithJs/calc/helper.js
+++ raw-examples/CppWithJs/calc/helper.js
@@ -4,8 +4,12 @@ function add(x, y) {
     return x + y;
 }
 
+function sub(x, y) {
+    return x - y;
+}
+
 module.exports = {
+    name: 'helper',
     add,
-    name: 'hi',
-    dict: { "a": "b" },
+    sub,
 }
--- raw-examples/CppWithJs/calc/index.js
+++ raw-examples/CppWithJs/calc/index.js
@@ -3,8 +3,10 @@
 const helper = require('helper.js');
 
 function main() {
-    PRINT(1024);
-    PRINT(helper.add(1024, 1));
+    const x = 1024;
+    PRINT(helper.sub(x, 1));
+    PRINT(x);
+    PRINT(helper.add(x, 1));
 }
 
 main();
